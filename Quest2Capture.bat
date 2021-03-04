3abfc86fbb80ce3dd58654873c1a889460f6da68
mitchv2020 <70819540+mitchv2020@users.noreply.github.com>
2021-03-04 01:31:56 +0000

    Add files via upload

---
+++ Quest2Capture.bat
@@ -0,0 +1,43 @@
+@echo off
+:start
+cls
+echo ==========================================
+echo Which capture commands do you want to run?
+echo ==========================================
+echo A) Singleplayer (1920x1080 90fps)
+echo B) Multiplayer  (1280x1280 90fps)
+echo ==========================================
+
+:start.2
+set INPUT=
+set /p INPUT=Answer: 
+if "%INPUT%"=="A" goto single
+if "%INPUT%"=="B" goto multi
+if "%INPUT%"=="a" goto single
+if "%INPUT%"=="b" goto multi
+Echo Please enter a valid answer!
+goto start.2
+
+:single
+cls
+adb shell setprop debug.oculus.capture.width 1920
+adb shell setprop debug.oculus.capture.height 1080
+adb shell setprop debug.oculus.capture.bitrate 10000000
+adb shell setprop debug.oculus.foveation.level 0
+adb shell setprop debug.oculus.fullRateCapture 1
+adb shell setprop debug.oculus.capture.fps 90
+Echo done.
+pause
+goto :start
+
+:multi
+cls
+adb shell setprop debug.oculus.capture.width 1280
+adb shell setprop debug.oculus.capture.height 1280
+adb shell setprop debug.oculus.capture.bitrate 10000000
+adb shell setprop debug.oculus.foveation.level 0
+adb shell setprop debug.oculus.fullRateCapture 1
+adb shell setprop debug.oculus.capture.fps 90
+Echo done.
+pause
+goto start
